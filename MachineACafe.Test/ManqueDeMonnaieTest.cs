e6913fc7d5ff2df4e69236558704f5dd734a932e
Nicolas Hamelin <n.hamelin44120@gmail.com>
2022-03-25 11:16:31 +0100

    test monnaie et test sucre

---
+++ MachineACafe.Test/ManqueDeMonnaieTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MachineACafe.Test
+{
+    [TestClass]
+    public class ManqueDeMonnaieTest
+    {
+        [TestMethod]
+        public void ManqueDeMonnaie()
+        {
+            // Arrange
+            const int monnaieInsérée = 39;
+            const int prixExpresso = 40;
+
+            // Act
+
+            // Assert
+            Assert.AreEqual(prixExpresso, monnaieInsérée, "Merci d'ajouter 1 cts");
+        }
+    }
+}
+
+/*
+ * ÉTANT DONNÉ que le client a inséré 39 cts
+ * QUAND le client appuie sur « expresso »
+ * ALORS la machine affiche « Merci d’ajouter 1 cts »
+*/
---
+++ MachineACafe.Test/SucreTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MachineACafe.Test
+{
+    [TestClass]
+    public class SucreTest
+    {
+        [TestMethod]
+        public void Sucre()
+        {
+
+        }
+    }
+}
+
+/*
+ * ÉTANT DONNÉ que le client a inséré 40 cts
+ * ET qu’il appuie sur « ajouter du sucre » { n } fois
+ * QUAND il appuie sur « expresso »
+ * ALORS la machine sert un expresso sucré { n } fois dans un gobelet jetable
+ * CASE { n } = 0, 1, 3, 5
+*/
