83b8e29e9eea9a33b3d78da40586b81b1f2fe2a6
Nicolas Hamelin <n.hamelin44120@gmail.com>
2022-03-25 10:16:45 +0100

    test

---
+++ MachineACafe.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace MachineACafe.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
